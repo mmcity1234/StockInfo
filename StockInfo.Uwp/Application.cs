b5ace73acf7c27740a56c9b9d75e33421f308347
ncclu <boodoo4321@gmail.com>
2018-02-12 09:37:26 +0800

    加入專案檔案。

---
+++ StockInfo.Uwp/Application.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MvvmCross.Platform.IoC;
+
+namespace StockInfo.Uwp
+{
+    public class Application : MvvmCross.Core.ViewModels.MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")           
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+
+            //RegisterAppStart<ViewModels.FirstViewModel>();
+        }
+    }
+}
---
+++ StockInfo.Uwp/Setup.cs
@@ -0,0 +1,24 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Uwp.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace StockInfo.Uwp
+{
+    public class Setup : MvxWindowsSetup
+    {
+        public Setup(Frame rootFrame) : base(rootFrame)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new Application();
+        }
+    }
+
+}
